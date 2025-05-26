e3c0f822d1d958bfe080fa063a0ab05eb0d31c51
Ratnakar-Test <155745698+Ratnakar-Test@users.noreply.github.com>
2025-05-26 16:58:21 +0530

    Update .eslintrc.js

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,32 +1,24 @@
-module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
+{
+  "env": {
+    "es6": true,
+    "node": true
   },
-  extends: [
+  "parserOptions": {
+    "ecmaVersion": 2020
+  },
+  "extends": [
     "eslint:recommended",
-    "google",
+    "google"
   ],
-  parserOptions: {
-    ecmaVersion: 2020, // or a more recent version like 2022 if using newer syntax
-    // sourceType: "module", // Uncomment if you are using ES modules (import/export)
-  },
-  rules: {
+  "rules": {
     "quotes": ["error", "double"],
-    "indent": ["error", 2, { "SwitchCase": 1 }],
+    "indent": ["error", 2],
+    "linebreak-style": ["off"], // Or ["warn", "unix"] to warn instead of error, or ["error", "unix"] if you fix them
     "object-curly-spacing": ["error", "always"],
-    "max-len": ["error", { "code": 120, "ignoreComments": true, "ignoreUrls": true }],
-    "require-jsdoc": "off", // Turn off JSDoc requirement
-    "valid-jsdoc": "off", // Turn off JSDoc validation
-    "operator-linebreak": ["error", "after"],
-    "linebreak-style": ["error", "unix"], // Enforce LF line endings
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-promise-reject-errors": ["error"],
-    "comma-dangle": ["error", "always-multiline"], // Optional: for cleaner git diffs
-    "arrow-parens": ["error", "always"], // Consistent parentheses for arrow functions
-    // Add or override other rules as needed
-    // e.g. if you use 'let' that could be 'const' but want to allow it:
-    // "prefer-const": "warn",
-  },
-};
+    "max-len": ["warn", { "code": 120, "ignoreUrls": true, "ignoreStrings": true, "ignoreTemplateLiterals": true }],
+    "require-jsdoc": "off", // If you don't want to enforce JSDoc comments
+    "valid-jsdoc": "off",   // If you don't want to enforce JSDoc comments
+    "new-cap": ["error", { "newIsCap": true, "capIsNew": false, "properties": true }],
+    "comma-dangle": ["error", "always-multiline"]
+  }
+}
